53c852f9a3d04bcf06ed12d5fd8608c40fb44278
Scott <clearplaid@gmail.com>
2019-06-16 17:27:03 -0400

    started orm object need to complete the query strings in order to retrieve the appropriate data from the db

---
+++ config/orm.js
@@ -0,0 +1,42 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+//need to complete query string parameters 
+        console.log(queryString);
+        
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+
+            console.log(result);
+            cb(result);
+        })
+    },
+
+    createOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+//need to complete query string parameters 
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+
+            cb(result);
+        });
+    },
+
+    updateOne: function(table, colVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+//need to complete query string parameters 
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+
+            cb(result);
+        })
+    }
+};
+
+module.exports = orm;
